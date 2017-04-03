d4da9e05cf6dc1868ef4709e2780499d734aa580
online1 <online1@ucione.local>
2017-04-03 21:04:22 +0200

    Add register page with Input class

--- index.php
+++ index.php
@@ -2,37 +2,7 @@
 	require_once 'core/init.php';
 	
 	Helper::getHeader('Algebra Contacts');
-	$db = DB::getInstance()->get('*', 'users');
-	//$db = DB::getInstance()->delete('users', array('id', '=', 7));
-	echo '<pre>';
-	var_dump($db);
-	/*
-	if($db->count() > 0) {
-		foreach ($db->results() as $result) {
-		echo $result->name;
-		}
-	} else {
-		echo 'Trenutno nema podataka u bazi!!!!';
-	}
-	
-	DB::getInstance()->get('*', 'users');
-	*/
-	 /*DB::getInstance()->insert('users', array(
-	 'username'=>'Marko',
-	 'password'=>'123456789',
-	 'salt'=>'65+464sdfg',
-	 'name'=>'Marko Markic',
-	 'role_id'=>1));*/
-	 
-	//"INSERT INTO users (name, username, password, salt) VALUES(?,?,?,?)"
-	
-	 /*DB::getInstance()->update('users', 7, array(
-		'name' => 'Iva Ivić',
-		'username' => 'Iva'
-	 ));*/
-	
-	//"UPDATE users SET name=?, username=? WHERE id=7"
-	
+		
 ?>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
