b5ef4631e8b0067420b35d27f39fd2d7b4820251
Aleksandar Dobrinić <dobrinich@gmail.com>
2017-04-12 16:42:53 +0200

    Added Password validation
    Rekao si da ćeš morati dodati validaciju od nekoga od nas pa evo.
    Kad si komentirao ovaj rad onaj prošli petak kad ti ništa nije išlo od ruke rekao si da ovo moje rješenje nije baš nešto jer kao da kako ćeš ti pronaći taj moj pattern - a kod ostalih da je bolje, a činjenica je da su ostali hardkodirali ,a ja sam spremio pattern u varijablu odn u polje. Pa brijem da si i tada bio zbunjen , te da si to krivo prokomentirao i da je moje rješenje ipak najbolje. 
    Istina da ni ovo nije optimalno rješenje  - ja bih te patterne spremao u KONSTANTE i smjestio ih negdje gdje su u cijeloj aplikaciji dostupne, ili odn u neki interfejs koji implementira klasa Validation.

--- classes/Validation.php
+++ classes/Validation.php
@@ -11,50 +11,65 @@ class Validation
 		$this->_db = DB::getInstance();
 	}
 	
-	public function check($items = array())
-	{
-		foreach ($items as $item => $rules) {
-			foreach($rules as $rule => $rule_value) {
-				
-				$item = escape($item);
-				$value = trim($_POST[$item]);
-				
-				if($rule === 'required' && empty($value)) {
-					$this->addError($item, "Field {$item} is required.");
-				} else if (!empty($value)) {
-					switch($rule) {
-						case 'min':
-							if(strlen($value) < $rule_value) {
-								$this->addError($item, "Field {$item} must have a minimum of {$rule_value} characters.");
-							}
-						break;
-						case 'max':
-							if(strlen($value) > $rule_value) {
-								$this->addError($item, "Field {$item} must have a maximum of {$rule_value} characters.");
-							}
-						break;
-						case 'matches':
-							if($value != $_POST[$rule_value]) {
-								$this->addError($item, "Field {$item} must match field {$rule_value}.");
-							}						
-						break;
-						case 'unique':
-							$check = $this->_db->get('id', $rule_value, array($item,'=',$value));
-							if($check->count()) {
-								$this->addError($item, "{$item} already exists.");
-							}
-						break;
-					}
-				}
-			}
-		}
-		
-		if(empty($this->_errors)) {
-			$this->_passed = true;
-		}
-		
-		return $this;
-	}
+		public function check($items = array())
+    	{
+    		foreach ($items as $item => $rules) {
+    			foreach($rules as $rule => $rule_value) {
+
+    				$item = escape($item);
+    				$value = trim($_POST[$item]);
+
+    				if($rule === 'required' && empty($value)) {
+    					$this->addError($item, "Field ".$this->format($item)." is required.");
+    				} else if (!empty($value)) {
+    					switch($rule) {
+    						case 'min':
+    							if(strlen($value) < $rule_value) {
+    								$this->addError($item, "Field ".$this->format($item)." must have a minimum of {$rule_value} characters.");
+    							}
+    						break;
+    						case 'max':
+    							if(strlen($value) > $rule_value) {
+    								$this->addError($item, "Field ".$this->format($item)." must have a maximum of {$rule_value} characters.");
+    							}
+    						break;
+    						case 'matches':
+    							if($value != $_POST[$rule_value]) {
+    								$this->addError($item, "Field ".$this->format($item)." must match field {$rule_value}.");
+    							}
+    						break;
+							/*
+                            			case 'pattern':
+    							if(!preg_match($rule_value ,$value)) {
+                                    				if ($item === 'password') {
+									$this->addError($item, "Field ".$this->format($item)." must include <b>at least</b> one <i>uppercase letter</i>, one <i>lowercase letter</i>, one <i>number</i>, and one <i>special character</i>.");
+								    }
+    							}
+    						break;*/
+    						case 'unique':
+    							$check = $this->_db->get('id', $rule_value, array($item,'=',$value));
+    							if($check->count()) {
+    								$this->addError($item, $this->format($item)." <i>".$value."</i> already exists.");
+    							}
+    						break;
+    					}
+    				}
+    			}
+    		}
+
+    		if(empty($this->_errors)) {
+    			$this->_passed = true;
+    		}
+
+    		return $this;
+    	}
+
+        private function format($item){
+            $item = str_replace("_", " ", $item);
+            $item = ucwords($item);
+
+            return "<b>".$item."</b>";
+        }
 	
 	private function addError($item, $error)
 	{
