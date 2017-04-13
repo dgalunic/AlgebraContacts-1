99be9378cd87d37d67de8f6c2581c40fca9fe763
Aleksandar Dobrinić <dobrinich@gmail.com>
2017-04-13 12:31:34 +0200

    Cleaning up my shit - sorry
    Zadnji puta sam krivo složio i nije pisao utf8 u bazu,, sad ispravljeno

--- classes/DB.php
+++ classes/DB.php
@@ -26,9 +26,10 @@ class DB
 		$this->_config = Config::get('config/database');
 		$driver = $this->_config['driver'];
 		$driverArray = $this->_config[$this->_config['driver']];
+		$dsn = $driver.':host='.$driverArray['host'].';dbname='.$driverArray['db'].';charset='.$driverArray['charset'];
 		
 		try {
-			$this->_connection = new PDO($driver.':host='.$driverArray['host'].';dbname='.$driverArray['db'].';'.$driverArray['charset'],$driverArray['user'], $driverArray['pass']);
+			$this->_connection = new PDO($dsn, $driverArray['user'], $driverArray['pass']);
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
